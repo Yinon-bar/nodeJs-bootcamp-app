902087230186e7cd9c1ee44260f27d3d99184d86
Yinon bar <yinonbar1988@gmail.com>
2022-12-18 15:21:48 +0200

    clean slate

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3004;
+
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "All Bootcamps" });
+});
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp!" });
+});
+
+app.listen(PORT, () => {
+  console.log("Listening to http:/localhost:3004 requests");
+});
