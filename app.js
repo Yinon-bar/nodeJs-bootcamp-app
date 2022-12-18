3943c17a1449c7b272a7b869eeba60993122f588
Yinon Bar <yinonbar1988@gmail.com>
2022-12-18 19:13:04 +0200

    clean slate

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const PORT = 3004;
 app.get("/api/v1/bootcamps", (req, res) => {
   res.status(200).json({ success: true, msg: "All Bootcamps" });
 });
+
 app.post("/api/v1/bootcamps", (req, res) => {
   res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
 });
 
 app.listen(PORT, () => {
