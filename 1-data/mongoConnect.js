cd3c48a8c5daa925a6a58e07d898dffe9c89abda
Yinon bar <yinonbar1988@gmail.com>
2022-12-19 08:40:45 +0200

    basic API no connection to db

--- 1-data/mongoConnect.js
+++ 1-data/mongoConnect.js
@@ -5,7 +5,6 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/bradTraversy");
   console.log("Connected to mongo!");
-  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
 
 module.exports = main;
